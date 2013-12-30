23727e40996095c30b119b403175d4eafe0897f6
zefanja <zefanja@freenet.de>
2013-12-30 14:50:40 +0700

    * fixed bug for modules with no sourceType config entry

--- scripts/filterMgr.js
+++ scripts/filterMgr.js
@@ -1,6 +1,6 @@
 define(["filters/osis", "filters/plain"], function (osis, plain) {
     function processText(inRaw, inSource, inDirection, inOptions) {
-        if(inSource.toLowerCase() === "osis")
+        if(inSource && inSource.toLowerCase() === "osis")
             return osis.processText(inRaw, inDirection, inOptions);
         else
             return plain.processText(inRaw, inDirection, inOptions);
